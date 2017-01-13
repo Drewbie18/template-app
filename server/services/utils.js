b48328046f3fb8a30cab70ee50a24392dd95f841
Drew Payette <apayette@ca.ibm.com>
2017-01-13 15:30:43 -0500

    added async test and expect lib

--- server/services/utils.js
+++ server/services/utils.js
@@ -8,5 +8,31 @@ module.exports.add = function (a, b) {
 
 
 module.exports.square = function (x) {
-    return x * x ;
-}
+    return x * x;
+};
+
+module.exports.setName = function (user, fullName) {
+    //create names array by splitting fullname at the space
+    var names = fullName.split(' ');
+    user.firstName = names[0];
+    user.lastName = names[1];
+    return user;
+};
+
+
+//mock async method
+module.exports.asyncAdd = function (a, b, callback) {
+    setTimeout(function () {
+        callback(a + b);
+    }, 1000)
+};
+
+
+module.exports.asyncFetch = function (name, callback) {
+
+    setTimeout(function () {
+        callback(name);
+    }, 500);
+
+
+};
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,3 +1,5 @@
+//assertion library
+const expect = require('expect');
 const utils = require('../server/services/utils');
 
 it('should add two numbers', function () {
@@ -17,3 +19,60 @@ it('should square the number', function () {
     }
 
 });
+
+
+//using assertion library
+it('should add two number together', function () {
+    var res = utils.add(4, 5);
+
+    //using the toBe assertion method
+    expect(res).toBe(9).toBeA('number');
+});
+
+
+it('should expect some values', function () {
+
+    expect(12).toNotBe(11);
+
+});
+
+
+//create test case on object method in utils
+it('should set the firstName and lastName fields', function () {
+
+    var user = {
+        age: 29,
+        location: 'Ottawa'
+    };
+
+    var res = utils.setName(user, 'Andrew Payette');
+    expect(res).toInclude({firstName: 'Andrew'}).toInclude({lastName: 'Payette'})
+
+
+});
+
+//must add the 'done' argument to the mocha callback to tell it that this is an async call
+//and that the test is not complete until done is called.
+//if it takes more than 2s mocha will assume it falied. 
+it('should add numbers async', function (done) {
+
+    utils.asyncAdd(5, 6, function (sum) {
+        expect(sum).toBe(11).toBeA('number');
+        done();
+    });
+});
+
+it('should return an object with name set', function (done) {
+
+    utils.asyncFetch('Drew', function (name) {
+        var user = {name: name};
+        expect(user).toInclude({name: 'Drew'});
+        done();
+        
+    });
+});
+
+
+
+
+
